e8957cb16fa3fb5129e8c9fa9b6a9c8db6f8c7ad
J Scott Chapman <scott8675309@gmail.com>
2018-08-02 20:33:53 -0600

    Add identity widget?

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Board from './Board';
 import registerServiceWorker from './registerServiceWorker';
+import netlifyIdentity from 'netlify-identity-widget';
 
+let buttons = document.createElement('div');
+netlifyIdentity.init();
 ReactDOM.render(<Board />, document.getElementById('root'));
 registerServiceWorker();
